6992467bf13bb4aeaa4cddc6b74e1901e6af0622
lorenzo <info@reedelk.com>
2020-06-18 20:05:00 +0200

    code

--- src/main/java/com/reedelk/mail/internal/script/MailAttachmentBuilder.java
+++ src/main/java/com/reedelk/mail/internal/script/MailAttachmentBuilder.java
@@ -3,13 +3,15 @@ package com.reedelk.mail.internal.script;
 import com.reedelk.runtime.api.annotation.Type;
 import com.reedelk.runtime.api.annotation.TypeFunction;
 
-@Type(
-        global = true,
+@Type(global = true,
         description = "The MailAttachmentBuilder creates new Mail Attachment object.")
 public class MailAttachmentBuilder {
 
     @TypeFunction(signature = "create()",
-            example = "MailAttachmentBuilder.create()",
+            example = "MailAttachmentBuilder.create()" +
+                    ".filename('my-picture.png')" +
+                    ".binary(message.payload(), 'image/png')" +
+                    ".build()",
             description = "Creates a new MailAttachment object.")
     public MailAttachment create() {
         return new MailAttachment();
