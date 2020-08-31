d2c659343ddaa8d4fec39eb9de23d7f254b9060f
lorenzo <info@reedelk.com>
2020-08-31 13:39:59 +0200

    bugfix: HTML email did not resolve inline resources (e.g images) correctly.

--- src/main/java/com/reedelk/mail/internal/smtp/type/MailWithHtml.java
+++ src/main/java/com/reedelk/mail/internal/smtp/type/MailWithHtml.java
@@ -6,8 +6,14 @@ import com.reedelk.runtime.api.flow.FlowContext;
 import com.reedelk.runtime.api.message.Message;
 import com.reedelk.runtime.api.message.content.MimeType;
 import com.reedelk.runtime.api.message.content.Pair;
+import org.apache.commons.mail.DataSourceResolver;
 import org.apache.commons.mail.EmailException;
 import org.apache.commons.mail.ImageHtmlEmail;
+
+import javax.activation.DataSource;
+import javax.activation.URLDataSource;
+import java.io.IOException;
+import java.net.URL;
 
 public class MailWithHtml extends AbstractMailType {
 
@@ -27,7 +33,22 @@ public class MailWithHtml extends AbstractMailType {
         Pair<String, String> charsetAndBody = buildCharsetAndMailBody(context, message);
         email.setCharset(charsetAndBody.left());
         email.setHtmlMsg(charsetAndBody.right());
+        email.setDataSourceResolver(new AbsoluteURLDataSourceResolver()); // resolve image sources inline.
 
         return MailTypeStrategyResult.create(email, charsetAndBody.right(), MimeType.TEXT_HTML);
+    }
+
+    // Resolves img src URLs when Email contains image URLs
+    private static class AbsoluteURLDataSourceResolver implements DataSourceResolver {
+
+        @Override
+        public DataSource resolve(String resourceLocation) throws IOException {
+            return new URLDataSource(new URL(resourceLocation));
+        }
+
+        @Override
+        public DataSource resolve(String resourceLocation, boolean isLenient) throws IOException {
+            return new URLDataSource(new URL(resourceLocation));
+        }
     }
 }
